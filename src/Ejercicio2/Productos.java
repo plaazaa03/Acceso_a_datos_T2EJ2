794c3f46b35592f67e68e85eb1f0f58b5a7f98e4
Raul <raulplaza208@gmail.com>
2023-11-21 16:33:15 +0100

    Empezado ejercicio 2

--- src/Ejercicio2/Productos.java
+++ src/Ejercicio2/Productos.java
@@ -1,5 +1,18 @@
 package Ejercicio2;
 
-public class Productos {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
+public class Productos {
+    public static void main(String[] args) throws FileNotFoundException {
+        try{
+            //Generar el fichero fproduct.dat
+            RandomAccessFile randomAccessFile = new RandomAccessFile("fproduct.dat", "rw");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
