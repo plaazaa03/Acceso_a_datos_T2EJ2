59aa4a5e90889e946ad39595b2da90bb0a0c17e9
Raul <raulplaza208@gmail.com>
2023-11-21 15:47:08 +0100

    Ejercicio 1 completo.

--- src/Ejercicio1/Bibilioteca.java
+++ src/Ejercicio1/Bibilioteca.java
@@ -1,4 +1,37 @@
 package Ejercicio1;
 
-public class Bibilioteca {
+import java.io.*;
+
+public class Bibilioteca  {
+    public static void main(String[] args) {
+        try {
+            //Flujos de entrada y salida
+            DataOutputStream salida = new DataOutputStream(new FileOutputStream("fichero.dat"));
+            DataInputStream entrada = new DataInputStream(new FileInputStream("fichero.dat"));
+
+
+            //Solicitar datos de libros y almacenarlos en el fichero
+            Libro libro1 = new Libro(1, "Don Quixote de la Mancha", "Miguel de Cervantes");
+            Libro libro2 = new Libro(2, "El niño del pijamas de rayas", "John Boyne");
+
+            libro1.grabarLibro(salida);
+            libro2.grabarLibro(salida);
+
+            salida.close();
+
+            //Leer el contenido del fichero mostrando en pantalla todos los libros insertados.
+            DataInputStream lectura = new DataInputStream(new FileInputStream("fichero.dat"));
+
+            while (entrada.available() > 0){
+                Libro libro = new Libro(0, "", "");
+                libro.leerLibro(entrada);
+                libro.mostrarLibro();
+                System.out.println("==========================");
+            }
+            entrada.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
